7b3198380b46ee33a8d133ec104c3e2af0ff61c5
Chidubem-Ifedigbo <chidubemifedigbo6@gmail.com>
2023-08-28 12:05:38 +0100

    monday morning commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1a4f6cd47f019634"; }
+/******/ 		__webpack_require__.h = function() { return "eea49ae5644d0b54"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
